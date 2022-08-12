acb998b537de17d3a8d3591b199e8ebe435839d9
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-08-12 16:44:22 -0700

    First Code Challenge Question Done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,26 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+// describe("stressed", () => {
+//     it ("create a string that returns relax if stressed and keep going if not" ,() => {
+//         expect(stressed("yes")).toEqual("relax")
+//         expect(stressed("no")).toEqual("keep going")
+//     })
+// })
+
+
+
+
+// const stressed = (string) => {
+//     if (string === "yes"){
+//         return "relax"
+//     }else{
+//         return "keep going"
+//     }
+// }
+
+
+
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -10,23 +31,22 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
+// --------------------Replaced the Instructor Example With the Template
 
-// a) Create a test with expect statements for each of the variables provided.
-
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
-
-
-// b) Create the function that makes the test pass.
-
-
+// // a describe method that lists the name of the function OR naming of the particular test.
+// describe("hello", () => {
+//     // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+//     it("returns a string that says hi", () => {
+//       //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+//       expect(hello()).toEqual("hi");
+//     });
+//   });
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
+
 // a) Create a test with expect statements for each of the variables provided.
+
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -35,9 +55,37 @@ const object2 =  { number: 0 }
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
+describe("divThree", () => {
+    it("determines if the number is divisible by 3 or not and tells you", () => {
+      expect(divThree(object1.number)).toEqual(`15 is divisible by three`);
+      expect(divThree(object2.number)).toEqual(`0 is divisible by three`);
+      expect(divThree(object3.number)).toEqual(`-7 is not divisible by three`);
+    });
+  });
+
+
+//   divThree
+//     ✕ determines if the number is divisible by 3 or not and tells you (2 ms)
+
+//   ● divThree › determines if the number is divisible by 3 or not and tells you
+
+//     ReferenceError: divThree is not defined
 
 // b) Create the function that makes the test pass.
 
+// Psuedo Code
+// Create a function named divThree
+// Input: take each variable and use the Modulo Method and see if its divisible by three (% 3)
+// Output: will return with a string telling you if the number is divisble by three or not
+
+
+const divThree = (number) => {
+    if (number % 3 === 0) {
+        return `${number} is divisible by three`
+    } else {
+        return `${number} is not divisible by three`
+    }
+}
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
